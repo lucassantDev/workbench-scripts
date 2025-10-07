e80a60de3e18b3cbd4335271f2d5c8bf3b5a8d33
Lucas Santana <lucasfdesantana095@gmail.com>
2025-10-07 00:41:08 -0300

    pasta com script mysql

---
+++ mysql-petshop/select-petshop.sql
@@ -0,0 +1,30 @@
+use petshop;
+
+#  relatório 01  - ordenado por  data de  admissão deforma  decrescente
+	select empregado.nome 'Empregado', 
+		empregado.cpf 'CPF do Empregado',  
+			date_format(empregado.dataAdm, '%d /%m /%Y')'Data de Admissão', 
+				concat('R$ ', format(empregado.salario, 2,  'de_DE')) 'Salário', 
+					#empregado.Departamento_idDepartamento 'Departamento',
+						departamento.nome 'Nome do Departamento',
+							telefone.numero  'Contato'
+			from empregado as empregado
+			left join departamento as departamento on departamento.idDepartamento = empregado.Departamento_idDepartamento
+            left join telefone as telefone on telefone.Departamento_idDepartamento = departamento.idDepartamento
+			order by empregado.dataAdm  desc;
+
+# relatório 02 - ordenado por empregador que ganham menos que a média salarial
+select empregado.nome 'Empregado', 
+		empregado.cpf 'CPF do Empregado',  
+			date_format(empregado.dataAdm, '%d /%m /%Y')'Data de Admissão', 
+				concat('R$ ', format(empregado.salario, 2,  'de_DE')) 'Salário', 
+					#empregado.Departamento_idDepartamento 'Departamento',
+						departamento.nome 'Nome do Departamento',
+							telefone.numero  'Contato'
+			from empregado as empregado
+			left join departamento as departamento on departamento.idDepartamento = empregado.Departamento_idDepartamento
+            left join telefone as telefone on telefone.Departamento_idDepartamento = departamento.idDepartamento
+            where empregado.salario < 3360;
+
+# descobrinco a  média salarial dos empregado referente ao relatório 02
+select format(avg(empregado.salario), 2, 'de_DE') 'Média Salarial' from empregado;
